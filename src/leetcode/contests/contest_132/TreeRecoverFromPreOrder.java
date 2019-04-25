714d78e9bb281b0ac719151f543b7af192c76830
Md Omar Faroque <faroque.ae@gmail.com>
2019-04-25 11:33:09 -0700

    cell distance

---
+++ src/leetcode/contests/contest_132/TreeRecoverFromPreOrder.java
@@ -0,0 +1,43 @@
+package leetcode.contests.contest_132;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import tree.TreeNode;
+
+public class TreeRecoverFromPreOrder {
+    TreeRecoverFromPreOrder recoverFromPreOrder;
+
+    @BeforeEach
+    public void init() {
+        recoverFromPreOrder = new TreeRecoverFromPreOrder();
+    }
+
+    @Test
+    public void firstTestWithSmallDataset(){
+        String input="1-2--3--4-5--6--7";
+        recoverFromPreOrder.recoverFromPreorder(input);
+    }
+    public TreeNode recoverFromPreorder(String S) {
+        TreeNode root=new TreeNode(0);
+        helper(root,0,S.length(),0,S);
+        return root;
+    }
+
+    private TreeNode helper(TreeNode root, int start, int end,int level,String input) {
+        return null;
+    }
+
+    private int findPositionForRightChildAtLevel(int level,String in) {
+        String str="";
+        for (int i=0;i<level;i++)
+            str+="-";
+        return in.lastIndexOf(str);
+    }
+
+    private int findPositionForLeftChildAtLevel(int level,String in) {
+        String str="";
+        for (int i=0;i<level;i++)
+            str+="-";
+        return in.lastIndexOf(str);
+    }
+}
---
+++ src/leetcode/contests/contest_133/MatrixCellDistance_1030.java
@@ -0,0 +1,33 @@
+package leetcode.contests.contest_133;
+
+import java.util.*;
+import java.util.stream.Stream;
+
+public class MatrixCellDistance_1030 {
+    public static void main(String[] args) {
+
+        allCellsDistOrder(2, 3, 1, 2);
+    }
+
+    public static int[][] allCellsDistOrder(int R, int C, int r0, int c0) {
+        int[][] result = new int[R * C][2];
+        TreeMap<Integer, List<Integer[]>> map = new TreeMap<>();
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                int dis = Math.abs(i - r0) + Math.abs(j - c0);
+                map.computeIfAbsent(dis, val -> new ArrayList<>()).add(new Integer[]{i, j});
+            }
+        }
+        int p = 0;
+        Iterator<Map.Entry<Integer, List<Integer[]>>> it = map.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry<Integer, List<Integer[]>> e = it.next();
+            List<Integer[]> value = e.getValue();
+            for (int i = 0; i < value.size(); i++) {
+                result[p] = new int[]{value.get(i)[0], value.get(i)[1]};
+                p++;
+            }
+        }
+        return result;
+    }
+}
