e6c3a3c3fdc363b7e2cb11abb727a961b820f35d
Md Omar Faroque <faroque.ae@gmail.com>
2019-04-26 18:40:34 -0700

    wrong submission

--- src/leetcode/contests/contest_133/TwoCitiesScheduling_1029.java
+++ src/leetcode/contests/contest_133/TwoCitiesScheduling_1029.java
@@ -1,43 +1,51 @@
 package leetcode.contests.contest_133;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class TwoCitiesScheduling_1029 {
     TwoCitiesScheduling_1029 twoCitiesScheduling;
+
+    private static class DistanceInterval {
+        int cityA;
+        int cityB;
+
+        public DistanceInterval(int cityA, int cityB) {
+            this.cityA = cityA;
+            this.cityB = cityB;
+        }
+    }
 
     @BeforeEach
     public void init() {
         twoCitiesScheduling = new TwoCitiesScheduling_1029();
     }
 
-    public int twoCitySchedCost(int[][] costs) {
-        Set<Integer> cityA=new HashSet<>();
-        Set<Integer> cityB=new HashSet<>();
-
-        for (int i = 0; i <costs.length ; i++) {
-            int[] c=costs[i];
-            if(c[0]<c[1])
-                cityA.add(i);
-            else if(c[0]>c[1])
-                cityB.add(i);
-            else{
-                if (cityA.size() < cityB.size())
-                    cityA.add(i);
-                else
-                    cityB.add(i);
-            }
-            if(cityA.size()==cityB.size()){
-                calcPrize(cityA,cityB,costs);
-            }
-
-        }
-        return 0;
+    @Test
+    public void firstTest() {
+        int[][] in = new int[][]{{10, 20}, {30, 200}, {400, 50}, {30, 20}};
+        int res= twoCitiesScheduling.twoCitySchedCost(in);
+        System.out.println(res);
     }
 
-    private void calcPrize(Set<Integer> cityA, Set<Integer> cityB, int[][] costs) {
+    public int twoCitySchedCost(int[][] costs) {
+        List<DistanceInterval> data = new ArrayList<>();
+        int n = costs.length / 2;
+        for (int i = 0; i < costs.length; i++) {
+            data.add(new DistanceInterval(costs[i][0], costs[i][1]));
+        }
+        data.sort(Comparator.comparingInt(d -> d.cityA));
+        int total=0;
+        for (int i = 0; i <n ; i++) {
+            total+=data.get(i).cityA;
+        }
 
+        List<DistanceInterval> newdata= data.subList(n,data.size());
+        for (int i = 0; i <newdata.size() ; i++) {
+            total+=newdata.get(i).cityB;
+        }
+        return total;
     }
 }
