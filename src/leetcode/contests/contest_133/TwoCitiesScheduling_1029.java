06f4f279c0e90a1d67f2d362ba4251b69e186bb8
Md Omar Faroque <faroque.ae@gmail.com>
2019-04-30 19:30:49 -0700

    code cleaning and test cases

--- src/leetcode/contests/contest_133/TwoCitiesScheduling_1029.java
+++ src/leetcode/contests/contest_133/TwoCitiesScheduling_1029.java
@@ -1,5 +1,6 @@
 package leetcode.contests.contest_133;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -7,8 +8,6 @@ import java.util.*;
 
 public class TwoCitiesScheduling_1029 {
     TwoCitiesScheduling_1029 twoCitiesScheduling;
-
-
     @BeforeEach
     public void init() {
         twoCitiesScheduling = new TwoCitiesScheduling_1029();
@@ -18,21 +17,25 @@ public class TwoCitiesScheduling_1029 {
     public void firstTest() {
         int[][] in = new int[][]{{259, 770}, {926, 667}, {577, 469}, {448, 54}, {184, 139}, {840, 118}};
         int res = twoCitiesScheduling.twoCitySchedCost(in);
-        System.out.println(res);
+        Assertions.assertEquals(1859,res);
     }
 
     public int twoCitySchedCost(int[][] costs) {
-        int n = costs.length / 2;
         Comparator<int[]> comparator = (a, b) -> Math.abs(b[0] - b[1]) - Math.abs(a[0] - a[1]);
-
-
         Arrays.sort(costs, comparator);
-
-
-        int N = costs.length/2, c1 = 0, c2 = 0, ans = 0, i = 0;
-        while(i < 2*N){
-            if((costs[i][0] <= costs[i][1] && c1 < N) || c2 == N){ans += costs[i++][0]; c1++;}
-            else{ans += costs[i++][1]; c2++;}
+        int N = costs.length / 2,
+                c1 = 0,// counter for the station A
+                c2 = 0,// counter for the station B
+                ans = 0,
+                i = 0;
+        while (i < 2 * N) {
+            if ((costs[i][0] <= costs[i][1] && c1 < N) || c2 == N) {
+                ans += costs[i++][0];
+                c1++;
+            } else {
+                ans += costs[i++][1];
+                c2++;
+            }
         }
         return ans;
     }
