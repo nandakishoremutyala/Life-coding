74006f3055ffb968d85251d08a63bffd6c4bef5b
Md Omar Faroque <faroque.ae@gmail.com>
2019-05-03 23:50:20 -0500

    commit segment tree impl

---
+++ src/practice/SegmentTree_050319.java
@@ -0,0 +1,27 @@
+package practice;
+
+import java.util.Arrays;
+
+public class SegmentTree_050319 {
+    static int[] in;
+    static int[] temp;
+
+    public static void main(String[] args) {
+        in = new int[]{5, 4, 3, 2, 7, 9, 10};
+        int len = in.length;
+        temp = new int[len + len];
+        for (int i = 0; i < in.length; i++) {
+            temp[i + len] = in[i];
+        }
+
+        System.out.println(2 << 1);
+        System.out.println(3 >> 1);
+
+        for (int i = len - 1; i > 0; --i)
+            temp[i] = Math.max(temp[i << 1], temp[i << 1 | 1]);
+
+        Arrays.stream(temp).forEach(value -> System.out.print(value));
+
+
+    }
+}
