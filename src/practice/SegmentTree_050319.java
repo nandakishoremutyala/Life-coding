b5717eda97b9dd4d8ec5730926a964175fbcbd5a
Md Omar Faroque <faroque.ae@gmail.com>
2019-05-04 12:07:11 -0500

    max element in range

--- src/practice/SegmentTree_050319.java
+++ src/practice/SegmentTree_050319.java
@@ -20,8 +20,15 @@ public class SegmentTree_050319 {
         for (int i = len - 1; i > 0; --i)
             temp[i] = Math.max(temp[i << 1], temp[i << 1 | 1]);
 
-        Arrays.stream(temp).forEach(value -> System.out.print(value));
+        Arrays.stream(temp).forEach(value -> System.out.print(value + "-"));
+        int res = searchInRange(len + 1, len + 5);
+        System.out.println(res);
+    }
 
-
+    public static int searchInRange(int start, int end) {
+        if (start == end) return temp[start];
+        end = end / 2;
+        start = start / 2;
+        return searchInRange(start, end);
     }
 }
