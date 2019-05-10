34b0a7df4e57c32a887e9bda06767028fda85e25
Md Omar Faroque <ofaroque@apple.com>
2019-05-09 18:21:59 -0700

    longest substring with distinct chars

---
+++ src/dailycodings/LongestSubsStringWithDistinctCharacter_DC_13.java
@@ -0,0 +1,31 @@
+package dailycodings;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestSubsStringWithDistinctCharacter_DC_13 {
+    public static void main(String[] args) {
+        String input = "mangopalllaa";
+        System.out.println(computeLength(input, 2));
+    }
+
+    private static int computeLength(String input, int k) {
+        int len = 0;
+        Set<Character> set = new HashSet<>();
+        int start = 0;
+        int end = 0;
+        while (end < input.length()) {
+
+            set.add(input.charAt(end));
+            if (set.size() > k) {
+                set.remove(input.charAt(start));
+                start++;
+            }
+            if (set.size() == k) {
+                len = Math.max((end - start)+1, len);
+            }
+            end++;
+        }
+        return len;
+    }
+}
