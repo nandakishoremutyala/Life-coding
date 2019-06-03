c098d2bed0aa414c8f189db8590c7e29ea8a08fe
Omar Faroque (Anik) <faroque.ae@gmail.com>
2019-06-02 19:23:44 -0500

    index pair

--- src/leetcode/contests/contest_139/IndexPairsOfString.java
+++ src/leetcode/contests/contest_139/IndexPairsOfString.java
@@ -6,25 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.*;
 
 public class IndexPairsOfString {
-
     IndexPairsOfString indexPairsOfString;
-
-    class Sample implements Comparable<Sample> {
-        private int start, end;
-
-        public Sample(int start, int end) {
-            this.start = start;
-            this.end = end;
-        }
-
-        public int compareTo(Sample o) {
-            if (o == null)
-                return -1;
-            return start - o.start;
-        }
-
-    }
-
 
     @BeforeEach
     public void init() {
@@ -40,33 +22,37 @@ public class IndexPairsOfString {
     public void testSecond() {
         int[][] result = indexPairsOfString.indexPairs("ababa", new String[]{"aba", "ab"});
     }
+
     private int[][] indexPairs(String text, String[] words) {
-        TreeMap<Integer,List<Integer>> map = new TreeMap<>();
+        TreeMap<Integer, List<Integer>> map = new TreeMap<>();
         for (int i = 0; i < words.length; i++) {
             String word = words[i];
             List<Integer> startPos = findWord(text, word);
             for (int j = 0; j < startPos.size(); j++) {
-                int s=startPos.get(j);
-                int e=s+word.length()-1;
-                map.computeIfAbsent(s,k->new ArrayList<>()).add(e);
+                int s = startPos.get(j);
+                int e = s + word.length() - 1;
+                map.computeIfAbsent(s, k -> new ArrayList<>()).add(e);
             }
         }
         System.out.println(map);
-        ArrayList<int[]> resultList=new ArrayList<>();
+        ArrayList<int[]> resultList = new ArrayList<>();
 
-        Iterator<Map.Entry<Integer, List<Integer>>> iterator=map.entrySet().iterator();
-        while (iterator.hasNext()){
-            Map.Entry<Integer,List<Integer>> entry=iterator.next();
-            int start=entry.getKey();
-            List<Integer> val=entry.getValue();
+        Iterator<Map.Entry<Integer, List<Integer>>> iterator = map.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<Integer, List<Integer>> entry = iterator.next();
+            int start = entry.getKey();
+            List<Integer> val = entry.getValue();
             Collections.sort(val);
             for (int i = 0; i < val.size(); i++) {
-                resultList.add(new int[]{start,val.get(i)});
+                resultList.add(new int[]{start, val.get(i)});
             }
         }
 
-        //System.out.println(resultList);
-        return (int[][]) resultList.toArray();
+        int[][] res = new int[resultList.size()][2];
+        for (int i = 0; i < resultList.size(); i++) {
+            res[i] = resultList.get(i);
+        }
+        return res;
     }
 
     private List<Integer> findWord(String textString, String word) {
