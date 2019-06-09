6af0bb131d4af3c9d5d610a6d0aefcc3b1ef34a2
Omar Faroque (Anik) <faroque.ae@gmail.com>
2019-06-09 03:08:17 -0500

    submission ready

--- src/leetcode/contests/contest_140/InsufficientNodeInRootToLeaf.java
+++ src/leetcode/contests/contest_140/InsufficientNodeInRootToLeaf.java
@@ -2,11 +2,14 @@ package leetcode.contests.contest_140;
 
 import leetcode.TreeNode;
 
+import java.util.HashSet;
+
 public class InsufficientNodeInRootToLeaf {
-    static TreeNode root;
+    static HashSet<TreeNode> toBeDeleted = new HashSet<>();
+    static int limit = 1;
 
     public static void main(String[] args) {
-        root = new TreeNode(1);
+        TreeNode root = new TreeNode(1);
 
         root.left = new TreeNode(2);
         root.right = new TreeNode(3);
@@ -15,7 +18,6 @@ public class InsufficientNodeInRootToLeaf {
         root.left.right = new TreeNode(-99);
         root.right.left = new TreeNode(-99);
         root.right.right = new TreeNode(7);
-
 
         root.left.left.left = new TreeNode(8);
         root.left.left.right = new TreeNode(9);
@@ -26,52 +28,46 @@ public class InsufficientNodeInRootToLeaf {
         root.right.left.right = new TreeNode(13);
         root.right.right.left = new TreeNode(-99);
         root.right.right.right = new TreeNode(14);
-        compute(root);
+        sufficientSubset(root, limit);
     }
 
-    private static void compute(TreeNode root) {
-        helper(root, 1, 0);
-        System.out.println(root);
-        makeValueNull(root);
-        System.out.println(root);
-
+    public static TreeNode sufficientSubset(TreeNode root, int limit) {
+        if (root == null)
+            return root;
+        editTree(root, limit);
+        if (toBeDeleted.contains(root))
+            root = null;
+        return root;
     }
 
-    private static void makeValueNull(TreeNode root) {
-        if(root==null)return;
-        if(root.val==Integer.MIN_VALUE)
-            nullify(root);
-        else{
-            makeValueNull(root.left);
-            makeValueNull(root.right);
-        }
-
-    }
-
-    private static void nullify(TreeNode root) {
-        if(root==null)return;
-        if(root.left==null && root.right==null)
-            root=null;
-        else{
-            nullify(root.left);
-            nullify(root.right);
-            //nullify(root);
-        }
-    }
-
-    private static void helper(TreeNode root, int limit, int sum) {
+    private static void editTree(TreeNode root, int limit) {
+        //1. if root is null, nothing to do
         if (root == null) return;
-        if ((root.left == null && root.right == null)) {
-            sum += root.val;
-            if (sum < limit)
-                root.val = Integer.MIN_VALUE;
-        }  else {
+        //2. We have to traverse upto the leaf, use recursion
+        editTree(root.left, limit - root.val);
+        editTree(root.right, limit - root.val);
+        //3. find the leaf
+        boolean isLeaf = root.left == null && root.right == null;
 
-            helper(root.left, limit, sum + root.val);
-            helper(root.right, limit, sum + root.val);
-            if ((root.left.val == Integer.MIN_VALUE && root.right.val == Integer.MIN_VALUE)) {
-                root.val = Integer.MIN_VALUE;
+        if (!isLeaf) {
+            //5. if left child is in the deleted list, make it null
+            if (toBeDeleted.contains(root.left)) {
+                toBeDeleted.remove(root.left);
+                root.left = null;
             }
+            //6. if right child is in the deleted list, make it null
+            if (toBeDeleted.contains(root.right)) {
+                toBeDeleted.remove(root.right);
+                root.right = null;
+            }
+            //7. if both child are null for the parent, make it in the delete list
+            if (root.left == null && root.right == null)
+                toBeDeleted.add(root);
         }
+        //4. if it is leaf, check the limit
+        // if root value is smaller than limit, add it to be deleted
+        else if (root.val < limit)
+            toBeDeleted.add(root);
+
     }
 }
