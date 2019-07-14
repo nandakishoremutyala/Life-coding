7d4f664b8b45320a6e50800a0798dc613395ce21
The Omar <faroque.ae@gmail.com>
2019-07-14 17:41:52 -0500

    code fixing

--- src/leetcode/contests/contest_145/LongestWellPerformingInterval.java
+++ src/leetcode/contests/contest_145/LongestWellPerformingInterval.java
@@ -4,66 +4,53 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+
 public class LongestWellPerformingInterval {
     LongestWellPerformingInterval mLongestWellPerformingInterval;
+
     @BeforeEach
-    public void init(){
-        mLongestWellPerformingInterval=new LongestWellPerformingInterval();
+    public void init() {
+        mLongestWellPerformingInterval = new LongestWellPerformingInterval();
     }
 
     @Test
-    public void firstTest(){
-        int[] hours=new int[]{9,9,6,1,6,6,9};
-        int result= mLongestWellPerformingInterval.longestWPI(hours);
-        Assertions.assertEquals(3,result);
+    public void firstTest() {
+        int[] hours = new int[]{9, 9, 6, 1, 6, 6, 9};
+        int result = mLongestWellPerformingInterval.longestWPI(hours);
+        Assertions.assertEquals(3, result);
     }
 
     @Test
-    public void secondTest(){
-        int[] hours=new int[]{6,6,9};
-        int result= mLongestWellPerformingInterval.longestWPI(hours);
-        Assertions.assertEquals(1,result);
+    public void secondTest() {
+        int[] hours = new int[]{6, 6, 9};
+        int result = mLongestWellPerformingInterval.longestWPI(hours);
+        Assertions.assertEquals(1, result);
     }
 
     @Test
-    public void thirdTest(){
-        int[] hours=new int[]{6,6,6};
-        int result= mLongestWellPerformingInterval.longestWPI(hours);
-        Assertions.assertEquals(0,result);
+    public void thirdTest() {
+        int[] hours = new int[]{6, 6, 6};
+        int result = mLongestWellPerformingInterval.longestWPI(hours);
+        Assertions.assertEquals(0, result);
     }
+
     public int longestWPI(int[] hours) {
-        int[][] traker=new int[hours.length][2];
-        if(hours[0]>8){
-            traker[0]=new int[]{1,0};
-        }else{
-            traker[0]=new int[]{0,1};
-        }
-
-        for (int i=1;i<hours.length;i++){
-            int pos=traker[i-1][0];
-            int neg=traker[i-1][1];
-            if(hours[i]>8){
-                traker[i]=new int[]{pos+1,neg};
-            }else{
-                traker[i]=new int[]{pos,neg+1};
+        int res = 0;
+        HashMap<Integer, Integer> seen = new HashMap<>();
+        int score = 0;
+        for (int i = 0; i < hours.length; i++) {
+            score += hours[i] > 8 ? 1 : -1;
+            if (score > 0)
+                res = i + 1;
+            else {
+                seen.putIfAbsent(score, i);
+                if (seen.containsKey(score - 1))
+                    res = Math.max(res, i - seen.get(score - 1));
             }
         }
 
-        System.out.println(traker);
-
-        int start=0;
-        int max=0;
-        for (int i=0;i<traker.length;i++){
-            if(traker[i][0]>traker[i][1]){
-                start=i;
-                while (i<traker.length && traker[i][0]>traker[i][1] ){
-                    i++;
-                }
-                max=Math.max(max,i-start+1);
-            }
-
-        }
-        return max;
+        return res;
     }
 
 }
