73c7e903bcf837b229db2f8c71e886c59278dc02
The Omar <faroque.ae@gmail.com>
2019-10-14 22:40:08 -0500

    minimum spanning tree

--- src/graph/MST.java
+++ src/graph/MST.java
@@ -12,18 +12,91 @@ public class MST {
         Graph(int V, int E) {
             this.V = V;
             this.E = E;
-            edge=new Edge[E];
-            for (int i = 0; i < E; i++) {
+            edge = new Edge[E];
+            for (int i = 0; i < E; ++i) {
                 edge[i] = new Edge();
             }
         }
 
         public void KruskalMST() {
+            Edge result[] = new Edge[V];  // Tnis will store the resultant MST
+            int e = 0;  // An index variable, used for result[]
+            int i = 0;  // An index variable, used for sorted edges
+            for (i=0; i<V; ++i)
+                result[i] = new Edge();
+
+            // Step 1:  Sort all the edges in non-decreasing order of their
+            // weight.  If we are not allowed to change the given graph, we
+            // can create a copy of array of edges
             Arrays.sort(edge);
-            for (Edge e : edge) {
-                System.out.println(e.src + "->" + e.dest+"..."+e.weight);
+
+            // Allocate memory for creating V ssubsets
+            subset subsets[] = new subset[V];
+            for(i=0; i<V; ++i)
+                subsets[i]=new subset();
+
+            // Create V subsets with single elements
+            for (int v = 0; v < V; ++v)
+            {
+                subsets[v].parent = v;
+                subsets[v].rank = 0;
             }
 
+            i = 0;  // Index used to pick next edge
+
+            // Number of edges to be taken is equal to V-1
+            while (e < V - 1)
+            {
+                // Step 2: Pick the smallest edge. And increment
+                // the index for next iteration
+                Edge next_edge = new Edge();
+                next_edge = edge[i++];
+
+                int x = find(subsets, next_edge.src);
+                int y = find(subsets, next_edge.dest);
+
+                // If including this edge does't cause cycle,
+                // include it in result and increment the index
+                // of result for next edge
+                if (x != y)
+                {
+                    result[e++] = next_edge;
+                    union(subsets, x, y);
+                }
+                // Else discard the next_edge
+            }
+
+            // print the contents of result[] to display
+            // the built MST
+            System.out.println("Following are the edges in " +
+                    "the constructed MST");
+            for (i = 0; i < e; ++i)
+                System.out.println(result[i].src+" -- " +
+                        result[i].dest+" == " + result[i].weight);
+        }
+
+        private void union(subset[] subsets, int x, int y) {
+            int xroot = find(subsets, x);
+            int yroot = find(subsets, y);
+
+            // Attach smaller rank tree under root of high rank tree
+            // (Union by Rank)
+            if (subsets[xroot].rank < subsets[yroot].rank)
+                subsets[xroot].parent = yroot;
+            else if (subsets[xroot].rank > subsets[yroot].rank)
+                subsets[yroot].parent = xroot;
+
+            else {
+                subsets[yroot].parent = xroot;
+                subsets[xroot].rank++;
+            }
+        }
+
+        private int find(subset[] subsets, int src) {
+            if (subsets[src].parent != src)
+                subsets[src].parent = find(subsets, subsets[src].parent);
+
+            return subsets[src].parent;
         }
     }
 
@@ -35,6 +108,11 @@ public class MST {
         public int compareTo(Edge compareEdge) {
             return this.weight - compareEdge.weight;
         }
+    }
+
+    static class subset {
+        int rank;
+        int parent;
     }
 
     public static void main(String[] args) {
