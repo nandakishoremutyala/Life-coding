8c92add012655bbda4f52799474bb89969ca3900
The Omar <faroque.ae@gmail.com>
2019-10-25 16:19:43 -0500

    largest area rectangle

--- src/practice/backtracking/LargestRectangleHistogram.java
+++ src/practice/backtracking/LargestRectangleHistogram.java
@@ -1,26 +1,27 @@
 package practice.backtracking;
 
+import org.junit.jupiter.api.Assertions;
+
 public class LargestRectangleHistogram {
     public static void main(String[] args) {
         LargestRectangleHistogram largestRectangleHistogram = new LargestRectangleHistogram();
         int res = largestRectangleHistogram.largestRectangleArea(new int[]{2, 1, 5, 6, 2, 3});
-        System.out.println(res);
-
+        Assertions.assertEquals(10, res);
         int res1 = largestRectangleHistogram.largestRectangleArea(new int[]{0, 9});
-        System.out.println(res1);
-
+        Assertions.assertEquals(9, res1);
         int res2 = largestRectangleHistogram.largestRectangleArea(new int[]{9, 0});
-        System.out.println(res2);
+        Assertions.assertEquals(9, res2);
     }
 
     public int largestRectangleArea(int[] heights) {
+        if (heights.length == 1) return heights[0];
         int maxArea = 0;
         for (int i = 0; i < heights.length - 1; i++) {
             int min = heights[i];
             for (int j = i + 1; j < heights.length; j++) {
                 min = Math.min(min, heights[j]);
                 maxArea = Math.max(heights[j], Math.max(maxArea, min * (j - i + 1)));
-                maxArea=Math.max(maxArea,heights[i]);
+                maxArea = Math.max(maxArea, heights[i]);
             }
         }
         return maxArea;
