0186b8f7894f65b72e074af0d0bea880a95518be
Omar Faroque (Anik) <faroque.ae@gmail.com>
2019-11-02 11:59:49 -0500

    possible bi partition

---
+++ src/data_structure/graph/dfs/PossibleBi_Partition.java
@@ -0,0 +1,53 @@
+package data_structure.graph.dfs;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * https://leetcode.com/problems/possible-bipartition/
+ */
+public class PossibleBi_Partition {
+    Map<Integer, Set<Integer>> graph = new HashMap<>();
+
+    //0-> node is not colored
+    //1 -> colored in group 1
+    //-1-> colored in group 2
+    public boolean possibleBipartition(int N, int[][] dislikes) {
+        buildGraph(graph, N, dislikes);
+        int[] colors = new int[N];
+        for (int node = 1; node <= N; node++) {
+            if (colors[node] == 0 && !dfs(graph, node, 1, colors))
+                return false;
+        }
+        return true;
+    }
+
+    private boolean dfs(Map<Integer, Set<Integer>> graph, int node, int color, int[] colors) {
+        if (colors[node] != 0) {
+            //conflict case
+            if (colors[node] == color) return true;
+            else return false;
+        }
+        //color the node
+        colors[node] = color;
+        // no child/ person does not dislike anyone
+        if (graph.get(node) == null) return true;
+        for (int p : graph.get(node)) {
+            // if there is any conflict in the down stream it has to return false
+            if (!dfs(graph, p, -color, colors)) return false;
+        }
+        return true;
+    }
+
+
+    private void buildGraph(Map<Integer, Set<Integer>> graph, int n, int[][] dislikes) {
+        for (int[] dislike : dislikes) {
+            int x = dislike[0];
+            int y = dislike[1];
+            graph.compute(x, (k, v) -> v == null ? new HashSet<>() : v).add(y);
+            graph.compute(y, (k, v) -> v == null ? new HashSet<>() : v).add(x);
+        }
+    }
+}
