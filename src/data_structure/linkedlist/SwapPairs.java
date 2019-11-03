3e4387336f48fe7e6f7e64e784792be41bcd9930
Omar Faroque (Anik) <faroque.ae@gmail.com>
2019-11-03 12:10:12 -0600

    swap pairs

---
+++ src/data_structure/linkedlist/SwapPairs.java
@@ -0,0 +1,33 @@
+package data_structure.linkedlist;
+
+import java.util.List;
+
+public class SwapPairs {
+
+    public static void main(String[] args) {
+        ListNode head=new ListNode(1);
+        head.next=new ListNode(2);
+        head.next.next= new ListNode(3);
+        head.next.next.next=new ListNode(4);
+        SwapPairs swapPairs=new SwapPairs();
+        ListNode res= swapPairs.swapPairs(head);
+        System.out.println(res.val);
+    }
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode current = dummy;
+        while (current.next != null && current.next.next != null) {
+            ListNode first = current.next;
+            ListNode second = current.next.next;
+
+            first.next = second.next;
+
+            current.next = second;
+
+            current.next.next = first;
+            current = current.next.next;
+        }
+        return dummy.next;
+    }
+}
