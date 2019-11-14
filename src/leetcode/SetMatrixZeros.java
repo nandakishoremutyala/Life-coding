d0300eed582de215ea177490ea878468e5d9f016
Omar Faroque (Anik) <faroque.ae@gmail.com>
2019-11-14 11:36:57 -0600

    set matrix zero

--- src/leetcode/SetMatrixZeros.java
+++ src/leetcode/SetMatrixZeros.java
@@ -2,36 +2,51 @@ package leetcode;
 
 public class SetMatrixZeros {
     public void setZeroes(int[][] matrix) {
-        int r = matrix.length;
-        int c = matrix[0].length;
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                if (matrix[i][j] == 0 || matrix[i][j] == Integer.MIN_VALUE)
-                    updateRowCol(i, j, r, c, matrix);
+        Boolean isCol = false;
+        int R = matrix.length;
+        int C = matrix[0].length;
+
+        for (int i = 0; i < R; i++) {
+
+            // Since first cell for both first row and first column is the same i.e. matrix[0][0]
+            // We can use an additional variable for either the first row/column.
+            // For this solution we are using an additional variable for the first column
+            // and using matrix[0][0] for the first row.
+            if (matrix[i][0] == 0) {
+                isCol = true;
+            }
+
+            for (int j = 1; j < C; j++) {
+                // If an element is zero, we set the first element of the corresponding row and column to 0
+                if (matrix[i][j] == 0) {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
             }
         }
 
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                if (matrix[i][j] == Integer.MAX_VALUE)
+        // Iterate over the array once again and using the first row and first column, update the elements.
+        for (int i = 1; i < R; i++) {
+            for (int j = 1; j < C; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
+                }
             }
         }
-    }
 
-    private void updateRowCol(int r, int c, int rows, int cols, int[][] matrix) {
-        for (int i = 0; i < cols; i++) {
-            if (matrix[r][i] == 0)
-                matrix[r][i] = Integer.MIN_VALUE;
-            else
-                matrix[r][i] = Integer.MAX_VALUE;
+        // See if the first row needs to be set to zero as well
+        if (matrix[0][0] == 0) {
+            for (int j = 0; j < C; j++) {
+                matrix[0][j] = 0;
+            }
         }
 
-        for (int i = 0; i < rows; i++) {
-            if (matrix[i][c] == 0)
-                matrix[i][c] = Integer.MIN_VALUE;
-            else
-                matrix[i][c] = Integer.MAX_VALUE;
+        // See if the first column needs to be set to zero as well
+        if (isCol) {
+            for (int i = 0; i < R; i++) {
+                matrix[i][0] = 0;
+            }
         }
     }
 }
+
