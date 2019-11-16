62c3ffa44f817e08f98f2e822590d2bef4684a4d
Omar Faroque (Anik) <faroque.ae@gmail.com>
2019-11-16 17:35:53 -0600

    similar string

---
+++ src/leetcode/LongestDuplicateSubstring.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+public class LongestDuplicateSubstring {
+    /**
+     *   b | a | n | a | n | a
+     * b 1 | 0 | 0 | 0 | 0 | 0
+     * a 1 | 1 | 0 | 1 | 0 | 1
+     * n 1 | 1 | 0 | 1 | 2 | 1
+     * a 1 | 1 | 0 | 1 | 2 | 3
+     * n
+     * a
+     **/
+    public static void main(String[] args) {
+
+    }
+}
