bfa02124b8aedb4da04d03d81e3d8ece04dbd67c
Omar Faroque (Anik) <faroque.ae@gmail.com>
2019-12-28 20:54:12 -0600

    all elements in a binary tree in ascending order

---
+++ src/leetcode/contests/contest_169/AllElementsInTwoBST.java
@@ -0,0 +1,55 @@
+package leetcode.contests.contest_169;
+
+import leetcode.TreeNode;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import others.MasterPrinter;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class AllElementsInTwoBST {
+    AllElementsInTwoBST allElementsInTwoBST;
+    @BeforeEach
+    public void init(){
+        this.allElementsInTwoBST=new AllElementsInTwoBST();
+    }
+
+    @Test
+    public void firstTest(){
+        TreeNode root1=new TreeNode(2);
+        root1.left=new TreeNode(1);
+        root1.right=new TreeNode(4);
+        TreeNode root2=new TreeNode(1);
+        root2.left=new TreeNode(0);
+        root2.right=new TreeNode(3);
+        List<Integer> res= allElementsInTwoBST.getAllElements(root1,root2);
+        MasterPrinter.printList(res);
+    }
+
+    @Test
+    public void secondTest(){
+        TreeNode root1=new TreeNode(0);
+        root1.left=new TreeNode(-10);
+        root1.right=new TreeNode(10);
+        TreeNode root2=null;
+        List<Integer> res= allElementsInTwoBST.getAllElements(root1,root2);
+        MasterPrinter.printList(res);
+    }
+
+    public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+        List<Integer> result=new ArrayList<>();
+        traverseTree(result,root1);
+        traverseTree(result,root2);
+        Collections.sort(result);
+        return result;
+    }
+
+    private void traverseTree(List<Integer> result, TreeNode root) {
+        if(root==null) return;
+        result.add(root.val);
+        traverseTree(result,root.left);
+        traverseTree(result,root.right);
+    }
+}
