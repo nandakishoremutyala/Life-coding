bbfa5ec92ae7fb574c0c9ed94896f2804be044c2
nanofaroque <omarf.android@gmail.com>
2020-05-09 17:08:51 -0500

    correction of the fourth problem

--- src/leetcode/contests/contest_187/KthSmallestSumOfAMatrix.java
+++ src/leetcode/contests/contest_187/KthSmallestSumOfAMatrix.java
@@ -20,35 +20,69 @@ public class KthSmallestSumOfAMatrix {
                 {1, 3, 11},
                 {2, 4, 6}
         };
-        int k=5;
-        int res= kthSmallest(matrix,k);
+        int k = 5;
+        int res = kthSmallest(matrix, k);
         System.out.println(res);
     }
 
     public int kthSmallest(int[][] mat, int k) {
-        PriorityQueue<Integer> pq=new PriorityQueue<>(Comparator.reverseOrder());
-        doBacktrack(mat,k,pq,0,0);
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        doBacktrack(mat, k, pq, 0, 0);
         System.out.println(pq);
         return pq.peek();
     }
-  /**
-   * 1  3  11
-   * 2  4  6
-   *
-   * */
+
+    /**
+     * 1  3  11
+     * 2  4  6
+     */
     private void doBacktrack(int[][] mat, int k, PriorityQueue<Integer> pq, int row, int sum) {
-        if(row==mat.length) {
+        if (row == mat.length) {
             pq.add(sum);
-            if(pq.size()>k)
+            if (pq.size() > k)
                 pq.poll();
             return;
         }
-        for (int i = 0; i <mat[0].length ; i++) {
-            sum+=mat[row][i];
-            System.out.println("growing sum: "+sum);
-            doBacktrack(mat,k,pq,row+1,sum);
-            sum-=mat[row][i];
-            System.out.println("reducing sum: "+sum);
+        for (int i = 0; i < mat[0].length; i++) {
+            sum += mat[row][i];
+            System.out.println("growing sum: " + sum);
+            doBacktrack(mat, k, pq, row + 1, sum);
+            sum -= mat[row][i];
+            System.out.println("reducing sum: " + sum);
         }
+    }
+
+    public int kthSmallestCorrection(int[][] mat, int k) {
+        int row = mat.length;
+        int col = mat[0].length;
+
+        // max priority queue for the first row
+        // take all the elements from the first row and add to the queue
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        for (int c = 0; c < col; c++) {
+            pq.add(mat[0][c]);
+            // keep pq size less than or equal to k
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+
+        for (int r = 1; r < row; r++) {
+            // max priority queue for the i-th row
+            PriorityQueue<Integer> nextPq = new PriorityQueue<>(Comparator.reverseOrder());
+            // for each of the element in the queue, take the next row element and add
+            // take only those element only upto to k size, so we can save some computation
+            for (int i : pq) {
+                for (int c = 0; c < col; c++) {
+                    nextPq.add(i + mat[r][c]);
+                    // keep pq size less than or equal to k
+                    if (nextPq.size() > k) {
+                        nextPq.poll();
+                    }
+                }
+            }
+            pq = nextPq;
+        }
+        return pq.poll();
     }
 }
