06a52b3d301be4e39a7ee2ff48563031f28f5497
nanofaroque <faroque.ae@gmail.com>
2021-07-30 18:44:06 -0500

    google interview

---
+++ src/prep_2021/all_googles/MaxNumberOfVisiblePoints.java
@@ -0,0 +1,35 @@
+package prep_2021.all_googles;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MaxNumberOfVisiblePoints {
+    public int visiblePoints(List<List<Integer>> points, int angle, List<Integer> location) {
+        List<Double> angles = new ArrayList<>();
+        int count = 0;
+        for (List<Integer> p : points) {
+            int dx = p.get(0) - location.get(0);
+            int dy = p.get(1) - location.get(1);
+            if (dx == 0 && dy == 0) {
+                count++;
+                continue;
+            }
+            angles.add(Math.atan2(dy, dx) * (180 / Math.PI));
+        }
+        Collections.sort(angles);
+        // handle edge cases
+        List<Double> temp = new ArrayList<>(angles);
+        for (double d : angles) {
+            temp.add(d + 360);
+        }
+        int res = count;
+        for (int i = 0, j = 0; i < temp.size(); i++) {
+            while (temp.get(i) - temp.get(j) > angle) {
+                j++;
+            }
+            res = Math.max(res, count + i - j + 1);
+        }
+        return res;
+    }
+}
