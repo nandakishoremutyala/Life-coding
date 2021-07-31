68d0c23905a66cfaec19ad2367ca1ccb3c43921f
nanofaroque <faroque.ae@gmail.com>
2021-07-31 17:23:17 -0500

    attendance

--- src/prep_2021/all_googles/StudentAttendanceRecordII.java
+++ src/prep_2021/all_googles/StudentAttendanceRecordII.java
@@ -1,12 +1,35 @@
 package prep_2021.all_googles;
 
-import java.util.Arrays;
-
 public class StudentAttendanceRecordII {
     public static void main(String[] args) {
         int res= checkRecord(10);
         System.out.println(res);
+        int result = checkRecordInDp(10);
+        System.out.println(result);
     }
+
+    private static int checkRecordInDp(int n) {
+        long M=1000000007;
+        long a0l0 = 1;
+        long a0l1 = 0, a0l2 = 0, a1l0 = 0, a1l1 = 0, a1l2 = 0;
+
+        for(int i=0;i<n;i++){
+            long new_a0l0 = (a0l0 + a0l1 + a0l2) % M;
+            long new_a0l1 = a0l0;
+            long new_a0l2 = a0l1;
+            long new_a1l0 = (a0l0 + a0l1 + a0l2 + a1l0 + a1l1 + a1l2) % M;
+            long new_a1l1 = a1l0;
+            long new_a1l2 = a1l1;
+            a0l0 = new_a0l0;
+            a0l1 = new_a0l1;
+            a0l2 = new_a0l2;
+            a1l0 = new_a1l0;
+            a1l1 = new_a1l1;
+            a1l2 = new_a1l2;
+        }
+        return (int)((a0l0 + a0l1 + a0l2 + a1l0 + a1l1 + a1l2) % M);
+    }
+
     static int count=0;
     public static int checkRecord(int n) {
         recursion(new StringBuilder("A0L0"),n,0);
