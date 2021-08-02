8d96bce3824b0c3a40ca3e5f2b38d4a74d3d326e
nanofaroque <faroque.ae@gmail.com>
2021-08-01 19:31:36 -0500

    min number of operations

---
+++ src/prep_2021/all_googles/MinNumberOfOperations.java
@@ -0,0 +1,26 @@
+package prep_2021.all_googles;
+
+import java.util.stream.IntStream;
+
+public class MinNumberOfOperations {
+    public static void main(String[] args) {
+        int res = minNumberOperations(new int[]{1, 2, 3, 2, 1});
+        System.out.println(res);
+    }
+
+    public static int minNumberOperations(int[] target) {
+        int forceToApply = target[0];
+        int totalForce = target[0];
+        for (int i = 1; i < target.length; i++) {
+            // if current chain strength is small, that means previous strength will break it
+            if (forceToApply >= target[i]) {
+                forceToApply = target[i];
+            } else {
+                // we have less force but diff has to be added
+                totalForce+=target[i]-forceToApply;
+                forceToApply= target[i];
+            }
+        }
+        return totalForce;
+    }
+}
