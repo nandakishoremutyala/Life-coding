0c186a6fab5486bc4d8172339ecdeb2d0648088d
nanofaroque <faroque.ae@gmail.com>
2021-08-14 14:37:13 -0500

    valid number

---
+++ src/effort_2021/ValidCharacter.java
@@ -0,0 +1,43 @@
+package effort_2021;
+
+import java.util.*;
+
+public class ValidCharacter {
+    public static void main(String[] args) {
+        ValidCharacter validCharacter=new ValidCharacter();
+        boolean res= validCharacter.isNumber(".1");
+        System.out.println(res
+        );
+    }
+    public boolean isNumber(String s) {
+        boolean seenDigit = false;
+        boolean seenExponent = false;
+        boolean seenDot = false;
+
+        for (int i = 0; i < s.length(); i++) {
+            char curr = s.charAt(i);
+            if (Character.isDigit(curr)) {
+                seenDigit = true;
+            } else if (curr == '+' || curr == '-') {
+                if (i > 0 && s.charAt(i - 1) != 'e' && s.charAt(i - 1) != 'E') {
+                    return false;
+                }
+            } else if (curr == 'e' || curr == 'E') {
+                if (seenExponent || !seenDigit) {
+                    return false;
+                }
+                seenExponent = true;
+                seenDigit = false;
+            } else if (curr == '.') {
+                if (seenDot || seenExponent) {
+                    return false;
+                }
+                seenDot = true;
+            } else {
+                return false;
+            }
+        }
+
+        return seenDigit;
+    }
+}
