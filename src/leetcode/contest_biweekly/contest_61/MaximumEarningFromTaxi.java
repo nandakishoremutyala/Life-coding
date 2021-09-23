e3c87272beae48993a914362fe44a2ee3abaa658
nanofaroque <faroque.ae@gmail.com>
2021-09-22 20:02:30 -0500

    max earning at taxi

---
+++ src/leetcode/contest_biweekly/contest_61/MaximumEarningFromTaxi.java
@@ -0,0 +1,33 @@
+package leetcode.contest_biweekly.contest_61;
+
+import java.util.*;
+
+public class MaximumEarningFromTaxi {
+    public static void main(String[] args) {
+        MaximumEarningFromTaxi maximumEarningFromTaxi = new MaximumEarningFromTaxi();
+       /* long res= maximumEarningFromTaxi.maxTaxiEarnings(20, new int[][]{{1, 6, 1}, {3, 10, 2}, {10, 12, 3}, {11, 12, 2}, {12, 15, 2}, {13, 18, 1}});
+        System.out.println(res);*/
+
+        long res1 = maximumEarningFromTaxi.maxTaxiEarnings(10, new int[][]{{9, 10, 2}, {4, 5, 6}, {6, 8, 1}, {1, 5, 5}, {4, 9, 5}, {1, 6, 5}, {4, 8, 3}, {4, 7, 10}, {1, 9, 8}, {2, 3, 5}});
+        System.out.println(res1);
+    }
+
+    public long maxTaxiEarnings(int n, int[][] rides) {
+        Arrays.sort(rides, Comparator.comparingInt(a -> a[0]));
+
+
+        int start = 0;
+        long[] cache = new long[n + 1];
+
+        while (start < rides.length) {
+            int[] ride = rides[start];
+            int src = ride[0];
+            int dest = ride[1];
+            int earn = ride[2];
+            cache[dest] = Math.max(cache[dest], cache[ride[0]] + dest - src + earn);
+            start++;
+        }
+
+        return 0;
+    }
+}
